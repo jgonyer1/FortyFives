ec52b49cd6bfa4db6c5a5572632de5bc18350339
Jacob Gonyer <jgonyer1@gmail.com>
2020-03-29 11:57:04 -0400

    restarting this as more of an api

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+import "dotenv/config";
+import express from "express";
+import cors from 'cors';
+const app = express();
+app.use(cors());
+app.listen(process.env.PORT, ()=>{
+    console.log(`Example app listening on port ${process.env.PORT}!`);
+});
+app.get("/", (req, res) =>{
+    res.send("Hello World!");
+})
